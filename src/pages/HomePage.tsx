099cae572e922a378ce3a44ba2323fecd7a00d5f
Jbrownsoldier <jamalevertonbrown@gmail.com>
2026-05-26 13:21:21 +0700

    refactor: remove ShingleVisualizer and replace slider with realistic Quebec freeze-thaw weathered shingles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import { Hero } from '../components/Hero';
 import { TrustBar } from '../components/TrustBar';
 import { HomeServicesPreview } from '../components/HomeServicesPreview';
 import { HiggsfieldExtendedSections } from '../components/HiggsfieldExtendedSections';
-import { ShingleVisualizer } from '../components/ShingleVisualizer';
 import { HomeReviewsPreview } from '../components/HomeReviewsPreview';
 import { SavingsCalculator } from '../components/SavingsCalculator';
 import { FAQ } from '../components/FAQ';
@@ -15,7 +14,6 @@ export function HomePage() {
       <TrustBar />
       <HomeServicesPreview />
       <HiggsfieldExtendedSections />
-      <ShingleVisualizer />
       <HomeReviewsPreview />
       <SavingsCalculator />
       <FAQ />
